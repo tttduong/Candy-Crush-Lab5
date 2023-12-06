a377caace79d8f86aa0b8e1071a10a67f4b52c8a
lehoangvi123 <100473303+lehoangvi123@users.noreply.github.com>
2023-12-06 20:34:52 +0700

    I add the update method
    I update the component of this method, the matching finding function.

--- Game.java
+++ Game.java
@@ -52,7 +52,86 @@ public class Game extends JPanel implements Runnable, MouseListener {
 
         grid[p1.row][p1.col] = p1;
         grid[p2.row][p2.col] = p2;
-    }
+    }  
+    
+    public void start () {
+        try {
+            view = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+            grid = new Piece[10][10];
+
+            background = ImageIO.read(getClass().getResource("/background.png"));
+            gems = ImageIO.read(getClass().getResource("/gems.png"));
+            cursor = ImageIO.read(getClass().getResource("/cursor.png"));
+
+            for(int i = 0;i < 10;i++) {
+                for(int j = 0;j < 10;j++) {
+                    grid[i][j] = new Piece();
+                }
+            }
+
+            for (int i = 1;i <=8;i++) {
+                for(int j = 1;j <= 8;j++) {
+                    grid[i][j].kind = (new Random().nextInt(7));
+                    grid[i][j].row = i;
+                    grid[i][j].col = j;
+                    grid[i][j].x = j * tileSize;
+                    grid[i][j].y = i * tileSize;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } 
+
+    public void update () {
+        if (mouse != null && mouse.getID() == MouseEvent.MOUSE_PRESSED) {
+            if (mouse.getButton() == MouseEvent.BUTTON1) {
+                if(!isSwap && !isMoving) {
+                    click++;
+                }
+                posX = mouse.getX() - offsetX;
+                posY = mouse.getY() - offsetY;
+
+                if (click == 1) {
+                    x0 = posX / tileSize + 1;
+                    y0 = posY / tileSize + 1;
+                }
+                if (click == 2) {
+                    x = posX / tileSize + 1;
+                    y = posY / tileSize + 1;
+                    if (abs(x - x0) + abs(y - y0) == 1) {
+                        swap(grid[y0][x0], grid[y][x]);
+                        isSwap = true;
+                        click = 0;
+                    } else {
+                        click = 1;
+                    }
+                }
+            }
+            mouse = null;
+        }
+
+        //Match finding
+        for (int i = 1;i <= 8;i++) {
+            for (int j = 1;j <= 8;j++) {
+                if (grid[i][j].kind == grid[i + 1][j].kind) {
+                    if (grid[i][j].kind == grid[i - 1][j].kind) {
+                        for (int n = -1;n <= 1;n++) {
+                            grid[i+n][j].match++;
+                        }
+                    }
+                }
+                if(grid[i][j].kind == grid[i][j + 1].kind) {
+                    if (grid[i][j].kind == grid[i][j - 1].kind) {
+                        for(int n = -1;n <= 1;n++) {
+                            grid[i][j + n].match++;
+                        }
+                    }
+                }
+            }
+        }
+    
     
     @Override
     public void mouseClicked(MouseEvent e) {
