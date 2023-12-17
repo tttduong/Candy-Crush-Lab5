7c918bd4788bff7774bf0ab73b958ba353d350bb
Thuy Duong <100646834+tttduong@users.noreply.github.com>
2023-12-17 18:13:29 +0700

    Add files via upload
    Add commend and funct hascode to Class Position

--- Position.java
+++ Position.java
@@ -1,14 +1,28 @@
-public class Position {
+import java.util.Objects;
 
+/**
+ * Position: vị trí điểm theo tọa độ xy
+ */
+public class Position {
+    /**
+     * XY coordinate (tọa độ)
+     */
     public int x;
-   public int y;
+
+    public int y;
 
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-
+    /**
+     *Compare the position of 2 objects.
+     * non-Object --> false
+     * khác x hoặc y, x và y --> false
+     * x=x && y=y  --> true
+     * Compares the Position object against another object.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -17,4 +31,12 @@ public class Position {
         return x == position.x && y == position.y;
     }
 
+    /**
+     * Function creates an object's hash representation
+     *Trả về biểu diễn hàm băm của object.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
