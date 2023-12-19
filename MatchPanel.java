fb10cf9d01f97da171b27a2fa0d4a48f3ee65ea6
The Crazy Dogg <147514211+theCrazyDogg@users.noreply.github.com>
2023-12-19 19:23:54 +0700

    add necessary variables, void drawRecentMatches() and paint()

--- MatchPanel.java
+++ MatchPanel.java
@@ -13,14 +13,28 @@ public class MatchPanel extends JPanel{
     private final int CELL_DIM = 32;
     private MatchBoard matchBoard;
     private Color[] colors = new Color[] {Color.BLUE, Color.BLACK, Color.YELLOW, Color.GREEN, Color.RED };
+    private GameState gameState;
+    private List<Position> recentMatches;
+    private List<Position> recentNewCells;
     
     public MatchPanel(int width, int height, Game game) {
         this.game = game;
         this.width = width;
         this.height = height;
+        matchBoard = new MatchBoard(width,height, colors.length);
+        recentMatches = new ArrayList<>();
+        recentNewCells = new ArrayList<>();
         setPreferredSize(new Dimension(width*CELL_DIM, height*CELL_DIM));
         setBackground(Color.gray);
 
+    }
+
+    //Hiển thị các thao tác gần đây, điểm số hiện tại và trạng thái đặc biệt khi kích hoạt pos1
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawRecentMatches(g);
+        drawAllCells(g);
     }
 
     private void drawAllCells(Graphics g) {
@@ -34,5 +48,17 @@ public class MatchPanel extends JPanel{
     private void drawCell(Graphics g, int x, int y) {
         g.setColor(colors[matchBoard.getCellValue(x,y)]);
         g.fillOval(x * CELL_DIM, y * CELL_DIM, CELL_DIM, CELL_DIM);
+    }
+
+    //Đánh dấu bằng các khung hình chữ nhật để hiển thị vị trí của những ô vừa ghép và những ô vừa được tạo mới ngẫu nhiên
+    private void drawRecentMatches(Graphics g) {
+        g.setColor(Color.ORANGE);
+        for(Position p : recentMatches) {
+            g.fillRect(p.x * CELL_DIM, p.y * CELL_DIM, CELL_DIM, CELL_DIM);
+        }
+        g.setColor(new Color(0,128,0));
+        for(Position p : recentNewCells) {
+            g.fillRect(p.x * CELL_DIM, p.y * CELL_DIM, CELL_DIM, CELL_DIM);
+        }
     }
 }
