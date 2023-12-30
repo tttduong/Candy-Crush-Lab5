cc1fe43b31eb7133f64b20675d3136dd6592645b
lehoangvi123 <100473303+lehoangvi123@users.noreply.github.com>
2023-12-30 11:04:32 +0700

    Update MatchPanel.java
    I update the Vietnamese comment in the MatchPanel.java

--- MatchPanel.java
+++ MatchPanel.java
@@ -9,80 +9,38 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Match Game
- * Author: Peter Mitchell (2021)
- *
- * MatchPanel class:
- * Shows a visual interactive panel representing a MatchBoard.
- * Receives clicks and propagates updates as the game continues.
- */
+//ở lớp matchpanel dùng để hiển thị bảng panel
+//và các thông tin sẽ được cập nhật trong matchpanel mỗi khi click chuột 
 public class MatchPanel extends JPanel implements MouseListener {
-    /**
-     * Reference to the Game object to pass score updates.
-     */
+    
+     //cập nhận điểm số thông qua cái object game
     private Game game;
-    /**
-     * States to enumerate through to determine how input is handled.
-     * ChoosePos1 is for setting the first Position for matching.
-     * ChoosePos2 comes after ChoosePos1 to set the second Position.
-     * After two valid positions are chosen PauseForDestroy pauses input until
-     * all matches are resolved. Then the game state returns to ChoosePos1.
-     * If an invalid position is selected for ChoosePos2, it will swap the selection to
-     * be handled with ChoosePos1 instead.
-     */
+    
     private enum GameState { ChoosePos1, ChoosePos2, PauseForDestroy }
-    /**
-     * Stores the GameState as described in GameState.
-     */
+    //lưu trữ game state
     private GameState gameState;
-    /**
-     * Positions for the match to be performed.
-     */
+    //vị trí 1,2 
     private Position pos1, pos2;
-    /**
-     * Number of cells horizontally (width), and vertically (height).
-     */
+    //số ô trống của chiều rộng và chiều cao 
     private int width, height;
-    /**
-     * Number of pixels for each cell representing both width and height.
-     */
+    //số điểm ảnh (pixel) khi mỗi ô biểu thị 
     private final int CELL_DIM = 32;
-    /**
-     * Colours to use for different number states in each cell of the matchBoard.
-     */
+    //màu hiển thị khác nhau đối với mỗi ô
     private Color[] colors = new Color[] { Color.BLUE, Color.BLACK, Color.YELLOW, Color.GREEN, Color.RED };
-    /**
-     * Reference to the MatchBoard containing the games board state for all cells.
-     */
+    
     private MatchBoard matchBoard;
-    /**
-     * The most recent matches that occurred. Can be rendered with a background using drawRecentMatches().
-     */
+    //ghi lại các match đã thành công hay chưa 
     private List<Position> recentMatches;
-    /**
-     * The most recent new cells that were added. Can be rendered with a background using drawRecentMatches().
-     */
+    //ghi lại các ô đã swap
     private List<Position> recentNewCells;
-    /**
-     * Timer for delaying match testing. The timer is configured with configureTimer()
-     * and is called when two valid positions have been matched to continue matching
-     * until there are no more matches.
-     */
+    //bộ hẹn giờ 
     private Timer stateTimer;
-    /**
-     * The current score based on the number of matches cells.
-     */
+    //ghi điểm hiện tại 
     private int score;
 
-    /**
-     * Initialises all the properties of the MatchPanel.
-     * Including setting up the MatchBoard object with a fresh board.
-     *
-     * @param width Number of cells horizontally.
-     * @param height Number of cells vertically.
-     * @param game Reference to the Game object for passing score updates.
-     */
+    
+
+    //bắt đầu khởi tạo các thuộc tính trong method matchpanel 
     public MatchPanel(int width, int height, Game game) {
         this.game = game;
         this.width = width;
@@ -99,12 +57,8 @@ public class MatchPanel extends JPanel implements MouseListener {
         score = 0;
     }
 
-    /**
-     * Draws all the recent activity, all the current cell values,
-     * and an overlay whenever a pos1 is set.
-     *
-     * @param g Reference to the Graphics area to draw on.
-     */
+    
+     // vẽ các ô trong game và điền element vô 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
@@ -115,12 +69,8 @@ public class MatchPanel extends JPanel implements MouseListener {
             drawSelectedPos1(g);
     }
 
-    /**
-     * Handles the clicks to set pos1 and pos2. After two Positions have been set
-     * the  matches are all evaluated until the player is given the ability to click again.
-     *
-     * @param e Event information about the MouseEvent that occurred.
-     */
+     // method này dùng để xử lí sau khi người chơi swap pos 1 và pos2 
+     //sau khi swap thì hệ thống sẽ cập nhật là ăn điểm hay là không
     @Override
     public void mouseClicked(MouseEvent e) {
         if(!(gameState == GameState.ChoosePos1 || gameState == GameState.ChoosePos2)) return;
@@ -144,9 +94,8 @@ public class MatchPanel extends JPanel implements MouseListener {
         repaint();
     }
 
-    /**
-     * Resets all the current board to a new puzzle and resets the score back to 0.
-     */
+    
+     //method để bắt đầu lại game nếu restart 
     public void restart() {
         score = 0;
         matchBoard.fillBoard();
@@ -154,47 +103,32 @@ public class MatchPanel extends JPanel implements MouseListener {
         repaint();
     }
 
-    /**
-     * Sets the value of pos1 and iterates the GameState on to choosing pos2.
-     *
-     * @param pos Position to store in pos1.
-     */
+    
+     //chọn ô thứ nhất và gamestate sẽ ghi lại, cập nhật dưới bảng output
     private void setPos1(Position pos) {
         pos1 = pos;
         gameState = GameState.ChoosePos2;
         System.out.println("Set pos1 as: " + pos.x + " " + pos.y);
     }
 
-    /**
-     * Sets the value of pos2 and iterates the GameState on to pausing while matching is completed.
-     *
-     * @param pos Position to store in pos2.
-     */
+    
+     //chọn ô thứ hai và gamestate sẽ ghi lại, cập nhật dưới bảng output  
     private void setPos2(Position pos) {
         pos2 = pos;
         gameState = GameState.PauseForDestroy;
         System.out.println("Set pos2 as: " + pos.x + " " + pos.y);
     }
 
-    /**
-     * Tests if the specified position is adjacent vertically or horizontally
-     * to the pos1 that has already been set.
-     *
-     * @param x X coordinate to test.
-     * @param y Y coordinate to test.
-     * @return True if passed position is adjacent to pos1.
-     */
+    
+     // kiểm tra xem nếu hai ô chọn có gần nhau vertically hay horizontally hay không
     private boolean isAdjacentToPos1(int x, int y) {
-        int diffx = Math.abs(x-pos1.x);
-        int diffy = Math.abs(y-pos1.y);
-        return (diffx == 0 || diffy == 0) && (diffx == 1 || diffy == 1);
+        int diffx = Math.abs(x-pos1.x); //vị trí x
+        int diffy = Math.abs(y-pos1.y); //vị trí y
+        return (diffx == 0 || diffy == 0) && (diffx == 1 || diffy == 1); //true nếu hai ô gần nhau 
     }
 
-    /**
-     * Iterates through all Positions on the board to make them draw.
-     *
-     * @param g Reference to the Graphics object for drawing.
-     */
+    
+     // check position mỗi ô để swap position    
     private void drawAllCells(Graphics g) {
         for(int x = 0; x < width; x++) {
             for(int y = 0; y < height; y++) {
@@ -203,15 +137,9 @@ public class MatchPanel extends JPanel implements MouseListener {
         }
     }
 
-    /**
-     * Attempts to perform the swap between pos1 and pos2.
-     * If there are no matches from the swap the cells are reset and the state
-     * returns to inputting the first position.
-     * If one or more matches are found the cells are swapped, then all cells
-     * are shuffled down to fill any gaps based on matches. Then cells at the top
-     * are filled in with new random values. Then the timer is started to continue matching
-     * until there are no more matches.
-     */
+
+    //thực hiện swap giữa hai position, nếu không khớp thì sẽ không swap và sẽ giữ nguyên position 
+    //nếu khớp thì sẽ swap và các ô sẽ rơi xuống để khớp với ô trống cho đến khi không còn match nữa 
     private void attemptCellSwap() {
         List<Position> matches = matchBoard.getMatchesFromSwap(pos1, pos2);
         updateScore(matches.size());
@@ -228,12 +156,7 @@ public class MatchPanel extends JPanel implements MouseListener {
         }
     }
 
-    /**
-     * Increases the score by amount and notifies the Game object that the
-     * score has changed. Does nothing if addAmount is not a positive number.
-     *
-     * @param addAmount The amount of score to increase by.
-     */
+    //method này dùng để cập nhật điểm 
     private void updateScore(int addAmount) {
         if(addAmount > 0) {
             score += addAmount;
@@ -243,13 +166,8 @@ public class MatchPanel extends JPanel implements MouseListener {
     }
 
     /**
-     * Draws a circle on the board at the relative board position for x,y.
-     * Colour is based on the array of colours and the number stored in that
-     * cell for MatchBoard.
-     *
-     * @param g Reference to the Graphics object for drawing.
-     * @param x X coordinate.
-     * @param y Y coordinate.
+     * vẽ hình oval cho mỗi cell, và hiển thị màu cho mỗi cell 
+     * @param g graphics object
      */
     private void drawCell(Graphics g, int x, int y) {
         g.setColor(colors[matchBoard.getCellValue(x,y)]);
@@ -257,10 +175,8 @@ public class MatchPanel extends JPanel implements MouseListener {
     }
 
     /**
-     * Draws rectangles to show where recent matches have occurred, and
-     * where recent new cells have been randomly filled in.
-     *
-     * @param g Reference to the Graphics object for drawing.
+     * 
+     * highlight cái match và highlight cái cell gần nhất đã fill vào ô trống
      */
     private void drawRecentMatches(Graphics g) {
         g.setColor(Color.ORANGE);
@@ -273,31 +189,27 @@ public class MatchPanel extends JPanel implements MouseListener {
         }
     }
 
-    /**
-     * Draws a cross with one vertical line and one horizontal line to show where the selected pos1 position is.
-     *
-     * @param g Reference to the Graphics object for drawing.
-     */
+    
+     
+     // đánh dấu cross ở postition 1 sau khi position 1 được chọn 
     private void drawSelectedPos1(Graphics g) {
         g.setColor(Color.WHITE);
         g.drawLine(pos1.x*CELL_DIM,pos1.y*CELL_DIM+CELL_DIM/2,pos1.x*CELL_DIM+CELL_DIM, pos1.y*CELL_DIM+CELL_DIM/2);
         g.drawLine(pos1.x*CELL_DIM+CELL_DIM/2,pos1.y*CELL_DIM,pos1.x*CELL_DIM+CELL_DIM/2, pos1.y*CELL_DIM+CELL_DIM);
     }
 
-    /**
-     * Draws a grid by drawing vertical lines and then horizontal lines based on the number of cells.
-     *
-     * @param g A reference to the Graphics object for rendering.
-     */
+    
+     
+     //vẽ grid dựa trên số ô được chọn 
     private void drawGrid(Graphics g) {
         g.setColor(Color.BLACK);
-        // Draw vertical lines
+        // vẽ một line dọc
         int y2 = 0;
         int y1 = height * CELL_DIM;
         for(int x = 0; x < width; x++)
             g.drawLine(x * CELL_DIM, y1, x * CELL_DIM, y2);
 
-        // Draw horizontal lines
+        // vẽ một line ngang 
         int x2 = 0;
         int x1 = width * CELL_DIM;
         for(int y = 0; y < height; y++)
@@ -305,24 +217,15 @@ public class MatchPanel extends JPanel implements MouseListener {
     }
 
     /**
-     * Tests if the position specified by x,y is inside the valid
-     * bounds of the panel.
-     *
-     * @param x X coordinate.
-     * @param y Y coordinate.
-     * @return True if the Position x,y is inside the MatchBoard.
+     * kiểm tra xem position có valid trong ô được chọn hay không 
+     * @return true nếu valid position 
      */
     private boolean isValidPosition(int x, int y) {
         if(x < 0 || y < 0 || x >= width || y >= height) return false;
         return true;
     }
 
-    /**
-     * Wipes out all matches without modifying the score.
-     * This is used for creating a start game state and for restarts.
-     * Every time matches are found, they are all directly filled with new
-     * random numbers.
-     */
+    //xóa match và cộng điểm, cell mới fill vô ô trống 
     private void createStableBoardState() {
         List<Position> matches = matchBoard.findAllMatches();
         while(!matches.isEmpty()) {
@@ -332,15 +235,9 @@ public class MatchPanel extends JPanel implements MouseListener {
         }
     }
 
-    /**
-     * Configures the stateTimer's actionPerformed event and delay.
-     * The stateTimer is set to not repeat and have 500ms delay.
-     * When the timer triggers it will find all matches on the board,
-     * then update and start the timer again. Once there are no more matches,
-     * the game state is swapped back to choosing pos1.
-     *
-     * The timer is ready to begin after this method by calling stateTimer.start();
-     */
+    //Bộ đếm thời gian kích hoạt, nó sẽ tìm thấy tất cả các kết quả phù hợp trên bảng.
+    //Sau đó cập nhật và bắt đầu lại bộ đếm thời gian.
+    //Một khi không còn trận đấu nào nữa, trạng thái trò chơi được hoán đổi trở lại việc chọn pos1.
     private void configureTimer() {
         stateTimer = new Timer(500, new ActionListener() {
             @Override
@@ -364,32 +261,21 @@ public class MatchPanel extends JPanel implements MouseListener {
         stateTimer.setRepeats(false);
     }
 
-    /**
-     * Not used.
-     *
-     * @param e Not used.
-     */
+    
+     // buộc phải có method mousePressed
     @Override
     public void mousePressed(MouseEvent e) {}
-    /**
-     * Not used.
-     *
-     * @param e Not used.
-     */
+    
+     // Buộc phải có mouseReleased 
     @Override
     public void mouseReleased(MouseEvent e) {}
-    /**
-     * Not used.
-     *
-     * @param e Not used.
-     */
+    
+     // buộc phải có method mouseEntered
     @Override
     public void mouseEntered(MouseEvent e) {}
-    /**
-     * Not used.
-     *
-     * @param e Not used.
-     */
+    
+     //buộc phải có mouseExited 
     @Override
     public void mouseExited(MouseEvent e) {}
+    
 }
